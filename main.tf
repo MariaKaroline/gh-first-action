10c1591f7bce55d2a1f2e49de9ffca90a975160b
Karol Gonçalves <79865026+MariaKaroline@users.noreply.github.com>
2023-08-08 15:45:59 -0300

    Correção referência errada

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_app_service_plan" "serviceplan" {
 resource "azurerm_app_service" "appservice" {
   name                = "app-service"
   location            = var.location
-  resource_group_name =  azurerm_resource_group.resourcegroupg.name
+  resource_group_name =  azurerm_resource_group.resourcegroup.name
   app_service_plan_id = azurerm_app_service_plan.serviceplan.id
 
   site_config {
