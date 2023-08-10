d5258d00498fecab92108e4144a268f3749476d2
Karol Gonçalves <79865026+MariaKaroline@users.noreply.github.com>
2023-08-10 14:21:04 -0300

    atualizando provider.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,12 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
 
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  tenant_id       = var.ARM_TENANT_ID
+}
 
 resource "azurerm_resource_group" "resourcegroup" {
   name     = "rg-teste"
